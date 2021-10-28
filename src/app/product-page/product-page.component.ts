a105f048a3a6fd9891376904f793f51eef3c7f87
cwainaina <cyrusnjuguna10@gmail.com>
2021-10-28 08:06:58 -0700

    release date class collected

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-page',
-  template: '../app/product-description.component.html',
+  templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent implements OnInit {
