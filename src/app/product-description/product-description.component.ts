661eadd2f0468ccf2c47df1834321b75b9223ba2
cwainaina <cyrusnjuguna10@gmail.com>
2021-10-28 07:52:35 -0700

    change on productDescription class

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -6,14 +6,14 @@ import { ProductService } from 'app/product.service';
   templateUrl: './product-description.component.html',
   styleUrls: ['./product-description.component.css']
 })
-export class ProductDescriptionComponent implements OnInit {
+export class ProductDescriptionComponent implements OnInit  {
   albumInfo;
 
-  constructor(private _productService) { }
+  constructor(private _productService:ProductService) { }
 
   ngOnInit() {
-    response =>this.albumInfo = response;
-    this._productService.getAlbum(1).subscribe();
+    this._productService.getAlbum(1).subscribe
+    (response =>this.albumInfo=response);
   }
 
 }
--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-page',
-  templateUrl: './product-page.component.html',
+  template: '../app/product-description.component.html',
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent implements OnInit {
