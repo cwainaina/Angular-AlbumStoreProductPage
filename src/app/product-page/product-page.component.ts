dcb28fc00d00d0f1e160cdca64319ddbb7360116
cwainaina <cyrusnjuguna10@gmail.com>
2021-10-29 08:16:14 -0700

    ng repetetionproductpage.html

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-page',
-  templateUrl: './product-page.component.html',
+  templateUrl: `./product-page.component.html
+  '
+  ,
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent implements OnInit {
